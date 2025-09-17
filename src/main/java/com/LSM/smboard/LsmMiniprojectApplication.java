2f45555ff9d34b893b4f772c4639020dd912c3db
LSM-1020 <smmlee@naver.com>
2025-09-17 16:17:25 +0900

    게시판 배포전

--- src/main/java/com/LSM/smboard/LsmMiniprojectApplication.java
+++ src/main/java/com/LSM/smboard/LsmMiniprojectApplication.java
@@ -2,12 +2,20 @@ package com.LSM.smboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 
 @SpringBootApplication
 
-public class LsmMiniprojectApplication {
+public class LsmMiniprojectApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		
+		return builder.sources(LsmMiniprojectApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LsmMiniprojectApplication.class, args);
